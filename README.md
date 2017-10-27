8382866777440a0b98d678dc7434402a246be7be
Tanzim Nawaz <tntanzim@gmail.com>
2017-10-27 19:58:48 +0600

    Table and Input from text file now
    Changed the code so the dfaTable and the inputSting, starting and final states are stored in separate text files. Also added explanation on how the DFA state  changing is working

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# DFA_Compare
+# DFA Compare
+A simple command line app to check if any given input string is valid for the given DFA table.
+
+# Usage
+There are two .txt files named dfaTable.txt and input.txt and the names are pretty self explanatory.
+
+Enter the desired DFA table in the first file.
+
+In the second file, provide the input string, starting state and ending state.
+
+There is already some testing data in the files. Please follow the format used in the files to get proper results.
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,7 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -5,23 +9,22 @@ import java.util.Objects;
 
 public class Main {
 
-    public static HashMap DFA = new HashMap();
-    public static List<String> terminals = new ArrayList <String>();
-    public static String inputString = "";
-    public static String startingState = "";
-    public static String finalState = "";
+    private static HashMap DFA = new HashMap();
+    //public static List<String> terminals = new ArrayList <String>();
+    private static BufferedReader bufferedReader;
+    private static String inputString;
+    private static String startingState;
+    private static String finalState;
 
     public static void main(String[] args) {
+        _input_();
         dfaTableConstruct();
         dfaCompare();
     }
 
-    public static void dfaCompare(){
-        inputString = "110111011";
-        startingState = "A";
-        finalState = "C";
+    private static void dfaCompare(){
         String currentState = startingState;
-        String nextState = startingState;
+        String nextState;
         char[] inputChar = inputString.toCharArray();
         System.out.println("***** Input *****\n" + inputString);
 
@@ -31,7 +34,15 @@ public class Main {
             String destination = (String) DFA.get(currentState);
             String[] splitDestination = destination.split("\\|");
 
-            nextState = splitDestination[Integer.parseInt(String.valueOf(inputChar[i]))];
+            /**The line of code below might need a bit of explanation
+             * The input string is comprised of '0's and '1's
+             * From any given state, there are 2 possible states to move to, Onw for 0 value and another for 1 value
+             * Also, destination states are stored in splitDestination array
+             * So here the code converts the '0'/'1' from string to integer and uses it as the array index
+             * This gives us a quick and easy way to determine the next state
+             * If the input string contains ever more numbers like 2,3,4 and so on, this code will still work
+             * If the input string contains a/b/c, then some modification is required*/
+            nextState = splitDestination[Integer.parseInt(String.valueOf(inputChar[i]))].trim();
 
             System.out.println("From " + currentState+" to " + nextState);
             currentState = nextState;
@@ -46,10 +57,45 @@ public class Main {
 
     }
 
-    public static void dfaTableConstruct(){
-        DFA.put("A","B|A");
-        DFA.put("B","A|C");
-        DFA.put("C","C|B");
+    private static void _input_(){
+        String input;
+        try {
+            bufferedReader = new BufferedReader(new FileReader("input.txt"));
+
+            /*Reads the first line of input.txt which is the input string*/
+            input = bufferedReader.readLine();
+            inputString = input.substring(12);
+
+            /*Reads the second line where the starting state is defined*/
+            input = bufferedReader.readLine();
+            startingState = input.substring(14);
+
+            /*Reads the third line where the final state is defined*/
+            input = bufferedReader.readLine();
+            finalState = input.substring(11);
+
+        } catch (FileNotFoundException e) {
+            System.out.println("input.txt file not found.");
+        } catch (IOException e) {
+            System.out.println("Can't read file input.txt");
+        }
     }
 
+    private static void dfaTableConstruct(){
+        String input;
+        String[] splitInput;
+        try {
+            bufferedReader = new BufferedReader(new FileReader("dfaTable.txt"));
+            input = bufferedReader.readLine();
+            while (input != null) {
+                splitInput = input.split("->");
+                DFA.put(splitInput[0].trim(), splitInput[1].trim());
+                input = bufferedReader.readLine();
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("dfaTable.txt File not found.");
+        } catch (IOException e) {
+            System.out.println("Can't read file dfaTable.txt");
+        }
+    }
 }
